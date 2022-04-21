9c4ea1bc3a9f4c0ea607b7f49b79ec011ad8b255
Sergey Svistun <svistunss.py@gmail.com>
2022-04-22 01:38:09 +0300

    inited eslint, scss, ts

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,14 @@
-/// <reference types="react-scripts" />
+declare module ".scss" {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module ".css" {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
